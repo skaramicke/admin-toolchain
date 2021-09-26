66d28e83c6cabe3b0c7f7a13c219d3f5c385223c
Mikael <skaramicke@gmail.com>
2021-09-26 09:02:30 +0200

    Add Reusable Blocks menu item tool

---
+++ tools/show_wp_blocks_menu_item.php
@@ -0,0 +1,52 @@
+<?php
+
+class Show_WP_Blocks_Menu_Item extends WPAT_Tool
+{
+    const TOOL_ID = 'wpat_show_wp_blocks_menu_item';
+    const OPTION_NAME = 'wpat_show_wp_blocks_menu_item';
+
+    public function __construct()
+    {
+        add_action('admin_init', function () {
+            register_setting($this::TOOL_ID, $this::OPTION_NAME);
+        });
+        if ($this->enabled()) {
+            add_action('admin_menu', [$this, 'add_menu_item']);
+        }
+    }
+
+    public function render_options()
+    {
+?>
+        <?php settings_fields($this::TOOL_ID); ?>
+        <?php do_settings_sections($this::TOOL_ID); ?>
+        <h2>Reusable Blocks</h2>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">Add menu item for Wordpress' Reusable Blocks</th>
+                <td><input type="checkbox" name="<?php echo $this::OPTION_NAME ?>" <?php echo $this->enabled() ? ' checked="checked"' : ""; ?>" /></td>
+            </tr>
+        </table>
+<?php
+    }
+
+    public function enabled()
+    {
+        return get_option($this::OPTION_NAME, false) == "on";
+    }
+
+    public function add_menu_item()
+    {
+        add_menu_page(
+            'Reusable Blocks',
+            'Reusable Blocks',
+            'edit_posts',
+            'edit.php?post_type=wp_block',
+            '',
+            'dashicons-editor-table',
+            22
+        );
+    }
+}
+
+$wpat_tool_instances['show_wp_blocks_menu_item'] = new Show_WP_Blocks_Menu_Item();
